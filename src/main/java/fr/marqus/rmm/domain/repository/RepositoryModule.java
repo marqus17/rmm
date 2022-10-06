afd6df09dbcdc145c222ad08a9890d4034fe543a
marqus <matthieu.marquigny@gmail.com>
2022-10-06 17:37:56 +0200

    Update DI module

--- src/main/java/fr/marqus/rmm/domain/repository/RepositoryModule.java
+++ src/main/java/fr/marqus/rmm/domain/repository/RepositoryModule.java
@@ -1,6 +1,7 @@
 package fr.marqus.rmm.domain.repository;
 
 import com.google.inject.AbstractModule;
+import fr.marqus.rmm.data.repository.MessageRepositoryImpl;
 import fr.marqus.rmm.data.repository.QueueConfigRepositoryImpl;
 
 /**
@@ -11,5 +12,6 @@ public final class RepositoryModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(QueueConfigRepository.class).to(QueueConfigRepositoryImpl.class);
+        bind(MessageRepository.class).to(MessageRepositoryImpl.class);
     }
 }
